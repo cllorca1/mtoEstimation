24e08fdb1798246a514c23aefce24f7f9fbbf2f5
cllorca1 <carlos.llorca@tum.de>
2017-06-26 15:52:27 +0200

    added data preparation for domestic

--- domestic/basicDomesticDestChoice.R
+++ domestic/basicDomesticDestChoice.R
@@ -37,7 +37,7 @@ matrixBusTt<-tt[]
 tt = travelTimes["data/rail"]
 matrixRailTt<-tt[]
 
-prices <- h5file("dataRome2Rio/transitTTModelEstV2.omx")
+prices <- h5file("dataRome2Rio/transitPriceModelEstV2.omx")
 tt = prices["data/auto"]
 matrixAutoPrice<-tt[]
 tt = prices["data/air"]
@@ -47,9 +47,32 @@ matrixBusPrice<-tt[]
 tt = prices["data/rail"]
 matrixRailPrice<-tt[]
 
+frequencies <- h5file("dataRome2Rio/transitFreqModelEstV2.omx")
+tt = frequencies["data/auto"]
+matrixAutoFreq<-tt[]
+tt = frequencies["data/air"]
+matrixAirFreq<-tt[]
+tt = frequencies["data/bus"]
+matrixBusFreq<-tt[]
+tt = frequencies["data/rail"]
+matrixRailFreq<-tt[]
 
-write.table(t(c(names(data), "alt", "choice" ,"td","tt.auto", "tt.air", "tt.bus", "tt.rail", "price.auto", "price.air", "price.bus", "price.rail", 
-                names(alternatives))), "processed/longData.csv", col.names = FALSE , sep = ",", row.names = FALSE)
+transfers <- h5file("dataRome2Rio/transitTransModelEstV2.omx")
+tt = transfers["data/auto"]
+matrixAutoTrans<-tt[]
+tt = transfers["data/air"]
+matrixAirTrans<-tt[]
+tt = transfers["data/bus"]
+matrixBusTrans<-tt[]
+tt = transfers["data/rail"]
+matrixRailTrans<-tt[]
+
+
+write.table(t(c(names(data), "alt", "choice" ,"td","tt.auto", "tt.air", "tt.bus", "tt.rail", 
+                "price.auto", "price.air", "price.bus", "price.rail", 
+                "freq.auto", "freq.air", "freq.bus", "freq.rail", 
+                "trans.auto", "trans.air", "trans.bus", "trans.rail", 
+                names(alternatives))), "processed/longData.csv", col.names = TRUE , sep = ",", row.names = FALSE)
 longData = data.frame()
 list1 = list()
 length(list1)=nrow(data)
@@ -66,7 +89,7 @@ for (i in 1:nrow(data)){
     dest <- trip$lvl2_dest 
     alt <- zone$zone_lvl2
     #coded as combined zone list
-    destCZId <- trip$lvl2_dest 
+    destCZId <- zone$zone_lvl2 
     origCZId <- trip$lvl2_orig 
     #read the skim matrices to get mode specific data
     dist <-tdMatrix[origCZId, destCZId]
@@ -78,6 +101,14 @@ for (i in 1:nrow(data)){
     priceAir = matrixAirPrice[origCZId, destCZId]
     priceBus= matrixBusPrice[origCZId, destCZId]
     priceRail = matrixRailPrice[origCZId, destCZId]
+    freqAuto = matrixAutoFreq[origCZId, destCZId]
+    freqAir = matrixAirFreq[origCZId, destCZId]
+    freqBus = matrixBusFreq[origCZId, destCZId]
+    freqRail = matrixRailFreq[origCZId, destCZId]
+    transAuto = matrixAutoTrans[origCZId, destCZId]
+    transAir = matrixAirTrans[origCZId, destCZId]
+    transBus= matrixBusTrans[origCZId, destCZId]
+    transRail = matrixRailTrans[origCZId, destCZId]
     
     if (alt==dest){
       choice = TRUE
@@ -85,21 +116,62 @@ for (i in 1:nrow(data)){
     #add here the distance from omx matrix and all mode specific variables
     #add here the price by car 
     row<-c(trip, alt = zone$combinedZone, choice = choice, td = dist, tt.auto = ttAuto, tt.air = ttAir,
-           tt.bus = ttBus, tt.rail= ttRail, price.auto = dist*0.072, price.air = priceAir, price.bus = priceBus, price.rail = priceRail, zone)
+           tt.bus = ttBus, tt.rail= ttRail, price.auto = dist*0.072, price.air = priceAir, 
+           price.bus = priceBus, price.rail = priceRail, 
+           freq.auto = freqAuto, freq.Air = freqAir, freq.bus = freqBus, freq.rail = freqRail, 
+           trans.Auto = transAuto, trans.Air = transAir, trans.bus = transBus, trans.rail = transRail,
+           zone)
     #write.table(row, file="longData.csv", append = TRUE, col.names = FALSE, sep="," )
     #alternatives<-rbind(alternatives, row)
     l[[j]] = row
     
   }
-  alternatives = rbindlist(l)
+  destinations = rbindlist(l)
   #write.table(alternatives, file="processed/longData.csv", append = TRUE, col.names = FALSE, sep="," , row.names = FALSE)
   #fwrite(x=alternatives, file="processed/longData.csv", append = TRUE, col.names = FALSE, sep="," , row.names = FALSE)
-  list1[[i]] = alternatives
+  list1[[i]] = destinations 
   
   print (paste("trip:", i , sep = " "))
   #longData<-rbind(longData, alternatives)
 }
 longData = rbindlist(list1)
-fwrite(x=longData, file="processed/longData.csv", append = TRUE, col.names = FALSE, sep="," , row.names = FALSE)
+fwrite(x=longData, file="processed/longData.csv", append = FALSE, col.names = TRUE, sep="," , row.names = FALSE)
+
+
+#read already generated data
+
+longData = fread(file="processed/longData.csv", header = T, sep = ',')
+wideData = subset(longData, choice == TRUE)
+
+#filter by purpose---
+
+##Leisure
+longData = subset(longData, purpose =="Leisure")
+wideData = subset(wideData, purpose =="Leisure")
+
+##Business
+longData = subset(longData, purpose =="Business")
+wideData = subset(wideData, purpose =="Business")
+
+##Visit
+longData = subset(longData, purpose =="Visit")
+wideData = subset(wideData, purpose =="Visit")
+
+
+#estimate DC
+
+library(mlogit)
+library(mnlogit)
+
+selectedAlts = unique(wideData$alt)
+longData <- subset(longData, alt %in% selectedAlts)
+
+longData$civic = longData$population + longData$employment
+
+weights = wideData$wtep
+f = formula(choice~ log(civic) + (hotel) + (sightseeing) + (outdoors) + (skiing) + exp(-0.003*td) | 0 |0)
+
+dcModel = mnlogit(data = longData, formula = f , choiceVar = "alt", weights = weights,  ncores=16, print.level = 2)
+summary(dcModel)
 
 
