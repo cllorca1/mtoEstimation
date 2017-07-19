f1df91be7a3595003f8102cc7e344fae739a54ae
cllorca1 <carlos.llorca@tum.de>
2017-07-19 17:13:41 +0200

    added some things in validation

--- validation/validationBorderCrossing.R
+++ validation/validationBorderCrossing.R
@@ -16,7 +16,21 @@ bcData = fread(file="surveyData.txt", header = T, sep = ',')
 bcDataRoutes = fread(file="routeInfo.txt", header = T, sep = ',')
 bcDataRoutes$travelDistance = bcDataRoutes$CAN + bcDataRoutes$US
 
+bc2Level4 = fread(file="zoning/bc2Level4.csv", header = T, sep = ',')
+model2Level4 = fread(file="zoning/model2Level4.csv", header = T, sep = ',')
+
 bc = full_join(x = bcData, y = bcDataRoutes, by = "RecordID")
+
+bcZone = bc2Level4$Zone
+compZone = bc2Level4$comparisonZone
+
+bc$orig = 0
+bc$dest = 0
+for (i in 1:length(bcZone)){
+  bc$orig[bc$OrigZone==bcZone[i]]=compZone[i]
+  bc$dest[bc$DestZone==bcZone[i]]=compZone[i]
+}
+
 
 purpTable = read.csv("purposeConversionTable.csv")
 purpTableActNo = purpTable$ActNo
@@ -26,11 +40,18 @@ for (i in 1:length(purpTableActNo)){
 bc$DestActNo[bc$DestActNo == purpTableActNo[i]] = as.character(purpTablePurp[i])
 }
 
+#to us
 surveyTrips = bc %>% filter(ResCountry == "Canada", Direction.x=="To USA", DestActNo != "na", TripFreqNo > 2) %>%
   select(dist = travelDistance, weight = Exp24Hr, from = ResCountry, to = Direction.x,purp = DestActNo)
 
+#to canada
 surveyTrips = bc %>% filter(ResCountry == "USA", Direction.x=="To USA", DestActNo != 1 , DestActNo != "na") %>%
   select(dist = travelDistance, weight = Exp24Hr, from = ResCountry, to = Direction.x, purp = DestActNo)
+
+#all
+
+surveyTrips = bc %>% filter(DestActNo != "na") %>%
+  select(dist = travelDistance, weight = Exp24Hr, from = ResCountry, to = Direction.x, purp = DestActNo, orig=orig, dest = dest)
 
 surveyTrips$to[surveyTrips$to == "To USA"] = "EXTUS"
 surveyTrips$to[surveyTrips$to == "To CAN"] = "ONTARIO"
@@ -39,7 +60,7 @@ surveyTrips$to[surveyTrips$to == "To CAN"] = "ONTARIO"
 
 #read model trips
 setwd("C:/models/mto/output")
-tripData <- read.csv("trips.csv", quote = "")
+tripData <- fread("trips.csv", quote = "")
 
 tripData$tripMode[tripData$tripMode==0] = "auto"
 tripData$tripMode[tripData$tripMode==1] = "air"
@@ -50,6 +71,14 @@ tripData$weight = 1
 tripData$weight[tripData$tripState == "inout"] = 0.5
 tripData$weight[tripData$tripState == "away"] = 0
 
+modelZones = model2Level4$id
+compZone = model2Level4$comparisonZone
+
+for (i in 1:length(modelZones)){
+  tripData$orig[tripData$tripOriginCombinedZone==modelZones[i]]=compZone[i]
+  tripData$dest[tripData$tripDestCombinedZone==modelZones[i]]=compZone[i]
+}
+
 
 #plot tt distribution
 tripData = subset(tripData, 
@@ -58,7 +87,7 @@ tripData = subset(tripData,
 
 
 modelTrips = tripData %>% filter(tripState!="away") %>%
-  select(dist = travelDistanceLvl2, weight = weight, from = tripOriginType, to = destZoneType, purp = tripPurpose)
+  select(dist = travelDistanceLvl2, weight = weight, from = tripOriginType, to = destZoneType, purp = tripPurpose, orig = orig, dest = dest)
 
 
 #merge all trips
@@ -69,15 +98,19 @@ modelTrips$source = "model"
 
 allTrips = rbind(surveyTrips, modelTrips)
 
-ggplot(subset(allTrips, to == "ONTARIO" & dist > 250), aes(x=dist, color = source,..density.., weights = weight)) + 
+ggplot(subset(allTrips, to == "ONTARIO"), aes(x=dist, color = source,..density.., weights = weight)) + 
  geom_freqpoly(binwidth = 100) + xlim(0,2000) + facet_grid(. ~ purp)
 
-
 ggplot(subset(allTrips, to == "EXTUS"), aes(x=dist, color = source,..density.., weights = weight)) + 
-  geom_freqpoly() + xlim(0,2000) + facet_grid(. ~ purp)
-
-ggplot(subset(allTrips, to == "EXTUS" & dist > 250), aes(x=dist, color = source,..density.., weights = weight)) + 
- geom_freqpoly() + xlim(0,2000) + facet_grid(. ~ purp)
+  geom_freqpoly(binwidth = 100) + xlim(0,2000) + facet_grid(. ~ purp)
 
 
+
+summary = allTrips  %>% group_by(source,orig,dest) %>% summarise(count = sum(weight))
+setwd("C:/projects/MTO Long distance travel/Choice Models/30 Validation")
+write.csv(x=summary, file = "tripsByOD.csv")
+
+
+x = tripData %>% filter(international=="true") %>% 
+  group_by (tripOriginCombinedZone, destZoneType) %>% summarize(count = n())
 
